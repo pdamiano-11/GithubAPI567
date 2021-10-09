6fe682a2d4863dd08b04d4043b871136564bbbc0
pdamiano-11 <pdamiano@stevens.edu>
2021-10-08 23:26:35 -0400

    hw05a_commit

--- test_hw04a.py
+++ test_hw04a.py
@@ -1,7 +1,19 @@
 import hw04a
+import mock
+from pytest_mock import mocker
+import pytest
 
-def test_response():
+def test_response(mocker):
+    mocker.patch('hw04a.getGitInfo', return_value = 18)
+    
     gitInfo = hw04a.getGitInfo('pdamiano-11')
-    assert 'pdamiano567' in gitInfo
-    assert gitInfo['SSW567'] != 3
-    assert gitInfo['SSW567-HW01'] == 2
+    expect = 18
+    assert gitInfo == expect
+    assert gitInfo != 15
+
+    mocker.patch('hw04a.getGitInfo', return_value = True)
+    
+    gitInfo = hw04a.getGitInfo('pdamiano-11')
+    expect = True
+    assert gitInfo == expect
+    assert gitInfo != False
